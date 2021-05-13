7092b3f65052ddd12bbbfbdf58cfd0115025aa89
Steven Pennington <steven.pennington@techelevator.com>
2021-05-13 10:29:15 -0400

    changed path for this computer

--- RunTestForJS.sh
+++ RunTestForJS.sh
@@ -3,14 +3,9 @@
 repoList="adamweaver adesuwaosagie alexanderleschkies aliceculin-ellison anitasirchia corrinscriber davidlearner jasoncoffman joncamerer joshuapeplow juliaengman kylehightower morganbrenner nataliejenkins parkerhorn patricestafford ratikgambhir ronfleming serorsalman sethumberger tylersteinman vilasinisarveswara"
 # Iterate thru the list running 'npm install' and 'npm run test' in each repo
 for curRepo in $repoList; do
-	cd ~/workspace/te/green-students/$curRepo/module-3/09_Event_Handling/exercise-student/shopping-list
+	cd ~/workspace/NLR2-student-repos/java-green/$curRepo/module-3/11_Introduction_to_Vue_and_Data_Binding/exercise-student
 	npm install
-	npm run test
-	echo
-	cd ..
-	cd calculator
-	npm install
-	npm run test
+	npm run test:unit
     echo
 	echo "Press Enter to continue..."
 	read
--- RunTestForMvn.sh
+++ RunTestForMvn.sh
@@ -3,7 +3,7 @@
 repoList="adamweaver adesuwaosagie alexanderleschkies aliceculin-ellison anitasirchia corrinscriber davidlearner jasoncoffman joncamerer joshuapeplow juliaengman kylehightower morganbrenner nataliejenkins parkerhorn patricestafford ratikgambhir ronfleming serorsalman sethumberger tylersteinman vilasinisarveswara"
 # Iterate thru the list running 'mvn test' in each repo
 for curRepo in $repoList; do
-	cd ~/workspace/te/green-students/$curRepo/module-1/03_Expressions/exercise-student/java
+	cd ~/workspace/NLR2-student-repos/java-green/$curRepo/module-1/03_Expressions/exercise-student/java
 	mvn test
 	echo
 	echo "Test results for $curRepo"
